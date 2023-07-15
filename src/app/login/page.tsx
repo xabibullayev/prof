dc8bf440783aa5a155876122ae96c86b68d6d1c4
xabibullayev <xabibullayevmm@gmail.com>
2023-07-15 17:15:14 +0500

    login page ready

---
+++ src/app/login/page.tsx
@@ -0,0 +1,32 @@
+import styles from "./page.module.scss";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import PortraitIcon from "@mui/icons-material/Portrait";
+import LockIcon from "@mui/icons-material/Lock";
+
+export default function Login() {
+  return (
+    <div className={styles.login}>
+      <div className={styles.container}>
+        <h1>Xosh keldin'iz!</h1>
+        <form>
+          <div className={styles.input}>
+            <span>
+              <PortraitIcon className={styles.icon} />
+            </span>
+            <input type="text" />
+          </div>
+          <div className={styles.input}>
+            <span>
+              <LockIcon className={styles.icon} />
+            </span>
+            <input type="password" />
+            <span>
+              <VisibilityOffIcon className={styles.icon} />
+            </span>
+          </div>
+          <button type="submit">Kiriw</button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,22 +11,30 @@ export default function Home() {
           <div className={styles.info}></div>
           <div className={styles.items}>
             <div className={styles.item}>
-              <img src="/images/home/home-1.png" alt="" />
-              <h1>Profilaktika inspektori</h1>
+              <Link href="/login">
+                <img src="/images/home/home-1.png" alt="" />
+                <h1>Profilaktika inspektori</h1>
+              </Link>
             </div>
             <div className={styles.item}>
-              <img src="/images/home/home-2.png" alt="" />
-              <h1>Hayal-qizlar inspektori</h1>
+              <Link href="/login">
+                <img src="/images/home/home-2.png" alt="" />
+                <h1>Hayal-qizlar inspektori</h1>
+              </Link>
             </div>
           </div>
           <div className={styles.items}>
             <div className={styles.item}>
-              <img src="/images/home/home-3.png" alt="" />
-              <h1>Inspektor psixolog</h1>
+              <Link href="/login">
+                <img src="/images/home/home-3.png" alt="" />
+                <h1>Inspektor psixolog</h1>
+              </Link>
             </div>
             <div className={styles.item}>
-              <img src="/images/home/home-3.png" alt="" />
-              <h1>Probaciya</h1>
+              <Link href="/login">
+                <img src="/images/home/home-3.png" alt="" />
+                <h1>Probaciya</h1>
+              </Link>
             </div>
           </div>
         </div>
